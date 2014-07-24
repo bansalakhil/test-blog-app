3a696f974cc2455757805639fc48e949443e9148
Akhil Bansal <bansalakhil30.10@gmail.com>
2014-07-24 17:22:00 +0530

    Using components little more

--- js/components/post_excerpt.js
+++ js/components/post_excerpt.js
@@ -1,4 +1,13 @@
 App.PostExcerptComponent = Ember.Component.extend({
 
-  classNames: ['user_post_excerpt']
+  classNames: ['user_post_excerpt'],
+
+  displayContent: false,
+
+  actions: {
+    toggleContent: function(){
+      this.toggleProperty("displayContent");
+    }
+  }
+
 })
