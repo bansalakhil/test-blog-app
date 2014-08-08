5528920d1a7442f64ed8f2ad95ec22a6b19d5661
Akhil Bansal <bansalakhil30.10@gmail.com>
2014-08-08 16:47:08 +0530

    Added notification to post create

--- js/controllers/posts_new_controller.js
+++ js/controllers/posts_new_controller.js
@@ -1,44 +1,57 @@
 App.PostsNewController = Ember.ObjectController.extend({
+  needs: ['application'],
+
   isEditing: false,
   selectedUser: null,
   // needs: ["users"],
   // users: Ember.computed.alias('controllers.users'),
 
-    actions: {
-        save: function() {
-            _this = this;
-            post = this.store.createRecord('post', {
+  actions: {
+    save: function() {
+      _this = this;
+      post = this.store.createRecord('post', {
 
-                title: this.get('title'),
-                content: this.get('body'),
-                user: this.get('selectedUser'),
-                excerpt: this.get('excerpt'),
-            });
-            
-            post.save().then(
+        title: this.get('title'),
+        content: this.get('body'),
+        user: this.get('selectedUser'),
+        excerpt: this.get('excerpt'),
+      });
 
-              function(data){
-              _this.resetForm();
-              _this.transitionToRoute('posts')
-              },
+      post.save().then(
 
-             function(data){
-              post.deleteRecord();
-              alert('Could not save record')
-              }
-            );
-            
+        function(data) {
+          post.deleteRecord();
+
+          _this.notifySuccess(post);
+          _this.resetForm();
+          _this.transitionToRoute('posts')
         },
-        cancel: function() {
-            this.resetForm();
-            this.transitionToRoute('posts');
 
-        },
+        function(data) {
+          _this.notifyFailure(post);
+          post.deleteRecord();
+          alert('Could not save record')
+        }
+      );
+
     },
-    resetForm: function() {
-        this.set('title', '');
-        this.set('excerpt', '');
-        this.set('body', '');
-        this.set('selectedUser', null);
+    cancel: function() {
+      this.resetForm();
+      this.transitionToRoute('posts');
+
     },
+  },
+  notifySuccess: function(post) {
+    console.log(this.get('controllers.application').send('pushNotification', 'success', 'Saved Successfully!!', 'Post ' + post.get('title') + ' saved on server successfully.'));
+  },
+  notifyFailure: function(post) {
+    console.log(this.get('controllers.application').send('pushNotification', 'fail', 'Failed!!!', 'Post ' + post.get('title') + ' could not be saved on server.'));
+  },
+
+  resetForm: function() {
+    this.set('title', '');
+    this.set('excerpt', '');
+    this.set('body', '');
+    this.set('selectedUser', null);
+  },
 })
